9776f14a1b34e672801d1866c8636aeb100142f6
Luiz Bassetto <lf.bassetto95@gmail.com>
2023-04-11 23:52:48 -0300

    upload main of typeconversion

---
+++ typeconversion_main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/Thelulutz/golang-beginning/typeconversion_main/typeconversion"
+
+func main() {
+
+	// call ConvertSpeed func
+	typeconversion.ConvertSpeed()
+
+}
